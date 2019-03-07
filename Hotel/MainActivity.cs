c849a154a3e2674b125008aa1de77a1b134605a4
hps3103 <ssingh.harpushpam@gmail.com>
2019-03-07 13:15:20 -0500

    Final Working Code

--- Hotel/MainActivity.cs
+++ Hotel/MainActivity.cs
@@ -12,12 +12,65 @@ namespace Hotel
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        
+        const int TAXPERCENT = 13;
+
+        EditText totalAmount, numberOfNightsReserved;
+        RadioButton suiteRoomBtn, doubleRoomBtn, singleRoomBtn;
+        CheckBox transportBox, spaBox, toursitGuideBox;
+        Button submitBtn;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
+
+            totalAmount = (EditText)FindViewById(Resource.Id.totalET);
+            numberOfNightsReserved = (EditText)FindViewById(Resource.Id.nightsET);
+            suiteRoomBtn = (RadioButton)FindViewById(Resource.Id.suiteRoomRadioGroup);
+            doubleRoomBtn = (RadioButton)FindViewById(Resource.Id.doubleRoomRadioGroup);
+            singleRoomBtn = (RadioButton)FindViewById(Resource.Id.singleRoomRadioGroup);
+            transportBox = (CheckBox)FindViewById(Resource.Id.transportCheckBox);
+            spaBox = (CheckBox)FindViewById(Resource.Id.spaCheckBox);
+            toursitGuideBox = (CheckBox)FindViewById(Resource.Id.guideCheckBox);
+            submitBtn = (Button)FindViewById(Resource.Id.okBtn);
+
+            double nightsReserved, totalBill =0.0;
+
+            submitBtn.Click += delegate
+            {
+
+                
+
+                if (!(suiteRoomBtn.Checked || doubleRoomBtn.Checked || singleRoomBtn.Checked))
+                    Toast.MakeText(this, "Please Specify a Room type", ToastLength.Long).Show();
+               
+
+                else
+                {
+                    nightsReserved = double.Parse(numberOfNightsReserved.Text);
+                    if (suiteRoomBtn.Checked)
+                        totalBill = 1400 * nightsReserved;
+
+                    else if (doubleRoomBtn.Checked)
+                        totalBill = 900 * nightsReserved;
+
+                    else if (singleRoomBtn.Checked)
+                        totalBill = 600 * nightsReserved;
+
+                    for (int i = 0; i < nightsReserved; i++)
+                    {
+                        if (spaBox.Checked)
+                            totalBill += (700);
+                        if (transportBox.Checked)
+                            totalBill += 1000;
+                        if (toursitGuideBox.Checked)
+                            totalBill += 2000;
+                    }
+                    totalBill += totalBill * 0.13;
+
+                    totalAmount.Text = totalBill.ToString();
+                }
+            };
 
 
         }
